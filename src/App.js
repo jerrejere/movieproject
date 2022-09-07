0358cf28ad6223e4e15685470ed9a12d16bf0ba4
jerryjohns <jerry.johns@student.moringaschool.com>
2022-09-07 11:08:43 +0300

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes,} from 'react-router-dom';
+import About from './About';
+import Hero from './Hero';
+import "../styles/App.css"
+import Doctors from './Doctors';
+import Form from './Form';
+import NavBar from './NavBar';
+import Footer from "./Footer"
+import ContactUs from './ContactUs';
+import '../styles/ContactUs.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container-fluid'>
+      <NavBar />
+      <Routes>
+        <Route exact path='/form' element={<Form/>}/>
+        <Route exact path='/about' element={<About/>}/>
+        <Route exact path='/doctors' element={<Doctors/>}/>
+        <Route exact path='/contactus' element={<ContactUs />} />
+        <Route exact path='/' element={<Hero/>}/>
+         
+      </Routes>
+      <Footer />
     </div>
   );
 }
